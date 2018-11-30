5a6571c88d9244f52bf48999b2f85a95c5637203
Bichuk <bichuk@ivk.ru>
2018-11-30 12:41:31 +0300

    fix : remove macro

--- ConcurrentHashMap.h
+++ ConcurrentHashMap.h
@@ -1,49 +1,86 @@
 #pragma once
 
-#include <Poco/RWLock.h>
+#include <shared_mutex>
 #include <algorithm>
 #include <unordered_map>
 
-#define DEFENCE_SOCPE(x, lockMode)            \
-  do {                                        \
-    if (mode == OperationMode::WITH_LOCK) {   \
-      _rwLock.lockMode();                     \
-    }                                         \
-    try {                                     \
-      x;                                      \
-    } catch (...) {                           \
-      if (mode == OperationMode::WITH_LOCK) { \
-        _rwLock.unlock();                     \
-      }                                       \
-      throw;                                  \
-    }                                         \
-    if (mode == OperationMode::WITH_LOCK) {   \
-      _rwLock.unlock();                       \
-    }                                         \
-  } while (0)
-
-#define DEFENCE_SOCPE_WITH_RESULT(res, x, lockMode) \
-  do {                                              \
-    if (mode == OperationMode::WITH_LOCK) {         \
-      _rwLock.lockMode();                           \
-    }                                               \
-    try {                                           \
-      res = x;                                      \
-    } catch (...) {                                 \
-      if (mode == OperationMode::WITH_LOCK) {       \
-        _rwLock.unlock();                           \
-      }                                             \
-      throw;                                        \
-    }                                               \
-    if (mode == OperationMode::WITH_LOCK) {         \
-      _rwLock.unlock();                             \
-    }                                               \
-  } while (0)
+template <typename F, typename Mtx>
+auto DefenceScopeReadLock(F f, Mtx &mtx, bool withLock) -> typename std::enable_if<std::is_same<decltype(f()), void>::value, void>::type {
+  if (withLock) {
+    mtx.lock_shared();
+  }
+  try {
+    f();
+  } catch (...) {
+    if (withLock) {
+      mtx.unlock_shared();
+    }
+    throw;
+  }
+  if (withLock) {
+    mtx.unlock_shared();
+  }
+}
+template <typename F, typename Mtx>
+auto DefenceScopeWriteLock(F f, Mtx &mtx, bool withLock) -> typename std::enable_if<std::is_same<decltype(f()), void>::value, void>::type {
+  if (withLock) {
+    mtx.lock();
+  }
+  try {
+    f();
+  } catch (...) {
+    if (withLock) {
+      mtx.unlock();
+    }
+    throw;
+  }
+  if (withLock) {
+    mtx.unlock();
+  }
+}
+template <typename F, typename Mtx>
+auto DefenceScopeReadLock(F f, Mtx &mtx, bool withLock) -> typename std::enable_if<!std::is_same<decltype(f()), void>::value, decltype(f())>::type {
+  decltype(f()) result;
+  if (withLock) {
+    mtx.lock_shared();
+  }
+  try {
+    result = f();
+  } catch (...) {
+    if (withLock) {
+      mtx.unlock_shared();
+    }
+    throw;
+  }
+  if (withLock) {
+    mtx.unlock_shared();
+  }
+  return result;
+}
+template <typename F, typename Mtx>
+auto DefenceScopeWriteLock(F f, Mtx &mtx, bool withLock) -> typename std::enable_if<!std::is_same<decltype(f()), void>::value, decltype(f())>::type {
+  decltype(f()) result;
+  if (withLock) {
+    mtx.lock();
+  }
+  try {
+    result = f();
+  } catch (...) {
+    if (withLock) {
+      mtx.unlock();
+    }
+    throw;
+  }
+  if (withLock) {
+    mtx.unlock();
+  }
+  return result;
+}
 
 template <typename Key, typename Value>
 class ConcurrentHashMap {
   mutable std::unordered_map<Key, Value> _map;
-  mutable Poco::RWLock _rwLock;
+  mutable std::shared_timed_mutex _rwLock;
 
  public:
   enum class OperationMode { WITH_LOCK, FORCE_NO_LOCK };
@@ -52,96 +89,134 @@ class ConcurrentHashMap {
   explicit ConcurrentHashMap(size_t n) : _map(n) {}
   ConcurrentHashMap(std::initializer_list<ValueType> list, size_t n = 0) : _map(list, n) {}
   ConcurrentHashMap(const ConcurrentHashMap &o) {
-    Poco::ScopedReadRWLock readRWLock(o._rwLock);
+    std::shared_lock<std::shared_timed_mutex> readRWLock(o._rwLock);
     _map = o._map;
   }
   ConcurrentHashMap(ConcurrentHashMap &&o) noexcept : _rwLock() {
-    Poco::ScopedWriteRWLock writeRWLock(o._rwLock);
+    std::unique_lock<std::shared_timed_mutex> writeRWLock(o._rwLock);
     _map = std::move(o._map);
   }
-  const Poco::RWLock &rwLock() const { return _rwLock; }
-  Poco::RWLock &rwLock() { return _rwLock; }
-  void insert(const Key &key, const Value &value, OperationMode mode = OperationMode::WITH_LOCK) { DEFENCE_SOCPE(_map.insert(std::make_pair(key, value)), writeLock); }
-  void emplace(Key &&key, Value &&value, OperationMode mode = OperationMode::WITH_LOCK) { DEFENCE_SOCPE(_map.emplace(std::forward<Key>(key), std::forward<Value>(value)), writeLock); }
+  const std::shared_timed_mutex &rwLock() const { return _rwLock; }
+  std::shared_timed_mutex &rwLock() { return _rwLock; }
+  void insert(const Key &key, const Value &value, OperationMode mode = OperationMode::WITH_LOCK) {
+    DefenceScopeWriteLock([&]() { _map.insert(std::make_pair(key, value)); }, _rwLock, mode == OperationMode::WITH_LOCK);
+  }
+  void emplace(Key &&key, Value &&value, OperationMode mode = OperationMode::WITH_LOCK) {
+    DefenceScopeWriteLock([&]() { _map.emplace(std::forward<Key>(key), std::forward<Value>(value)); }, _rwLock, mode == OperationMode::WITH_LOCK);
+  }
   bool empty(OperationMode mode = OperationMode::WITH_LOCK) const {
-    bool result;
-    DEFENCE_SOCPE_WITH_RESULT(result, _map.empty(), readLock);
-    return result;
+    return DefenceScopeReadLock([&]() -> bool { return _map.empty(); }, _rwLock, mode == OperationMode::WITH_LOCK);
   }
   size_t size(OperationMode mode = OperationMode::WITH_LOCK) const {
-    size_t result;
-    DEFENCE_SOCPE_WITH_RESULT(result, _map.size(), readLock);
-    return result;
+    return DefenceScopeReadLock([&]() -> size_t { return _map.size(); }, _rwLock, mode == OperationMode::WITH_LOCK);
   }
   bool contains(const Key &k, OperationMode mode = OperationMode::WITH_LOCK) const {
-    bool result;
-    DEFENCE_SOCPE_WITH_RESULT(result, _map.find(k) != _map.end(), readLock);
-    return result;
+    return DefenceScopeReadLock([&]() -> bool { _map.find(k) != _map.end(); }, _rwLock, mode == OperationMode::WITH_LOCK);
   }
-  void erase(const Key &key, OperationMode mode = OperationMode::WITH_LOCK) { DEFENCE_SOCPE(_map.erase(key), writeLock); }
+  void erase(const Key &key, OperationMode mode = OperationMode::WITH_LOCK) {
+    DefenceScopeWriteLock([&]() { _map.erase(key); }, _rwLock, mode == OperationMode::WITH_LOCK);
+  }
   void eraseKeys(const std::vector<Key> &keys, OperationMode mode = OperationMode::WITH_LOCK) {
-    DEFENCE_SOCPE(for (const auto &k : keys) { _map.erase(k); }, writeLock);
+    DefenceScopeWriteLock(
+        [&]() {
+          for (const auto &k : keys) {
+            _map.erase(k);
+          }
+        },
+        _rwLock,
+        mode == OperationMode::WITH_LOCK);
   }
   template <typename Predicate>
   void eraseIf(Predicate predicate, OperationMode mode = OperationMode::WITH_LOCK) {
-    DEFENCE_SOCPE(
-        for (auto it = _map.begin(); it != _map.end();) {
-          if (predicate(*it)) {
-            it = _map.erase(it);
-          } else {
-            ++it;
+    DefenceScopeWriteLock(
+        [&]() {
+          for (auto it = _map.begin(); it != _map.end();) {
+            if (predicate(*it)) {
+              it = _map.erase(it);
+            } else {
+              ++it;
+            }
           }
         },
-        writeLock);
+        _rwLock,
+        mode == OperationMode::WITH_LOCK);
   }
   template <typename Predicate>
   void eraseKeyIf(Predicate predicate, const Key &key, OperationMode mode = OperationMode::WITH_LOCK) {
-    DEFENCE_SOCPE(auto item = _map.find(key); if (item != _map.end() && predicate(*item)) { _map.erase(item); }, writeLock);
+    DefenceScopeWriteLock(
+        [&]() {
+          auto item = _map.find(key);
+          if (item != _map.end() && predicate(*item)) {
+            _map.erase(item);
+          }
+        },
+        _rwLock,
+        mode == OperationMode::WITH_LOCK);
   }
   template <typename Function>
   void doForEeach(Function function, OperationMode mode = OperationMode::WITH_LOCK) const {
-    DEFENCE_SOCPE(std::for_each(_map.begin(), _map.end(), function), readLock);
+    DefenceScopeReadLock([&]() { std::for_each(_map.begin(), _map.end(), function); }, _rwLock, mode == OperationMode::WITH_LOCK);
   }
   template <typename Function>
   void applyForEeach(Function function, OperationMode mode = OperationMode::WITH_LOCK) {
-    DEFENCE_SOCPE(std::for_each(_map.begin(), _map.end(), function), writeLock);
+    DefenceScopeWriteLock([&]() { std::for_each(_map.begin(), _map.end(), function); }, _rwLock, mode == OperationMode::WITH_LOCK);
   }
   template <typename Function, typename Predicate>
   void doForEeachIf(Function function, Predicate predicate, OperationMode mode = OperationMode::WITH_LOCK) const {
-    DEFENCE_SOCPE(
-        for (const auto &item
-             : _map) {
-          if (predicate(item)) {
-            function(item);
+    DefenceScopeReadLock(
+        [&]() {
+          for (const auto &item : _map) {
+            if (predicate(item)) {
+              function(item);
+            }
           }
         },
-        readLock);
+        _rwLock,
+        mode == OperationMode::WITH_LOCK);
   }
   template <typename Function, typename Predicate>
   void applyForEeachIf(Function function, Predicate predicate, OperationMode mode = OperationMode::WITH_LOCK) {
-    DEFENCE_SOCPE(
-        for (auto &item
-             : _map) {
-          if (predicate(item)) {
-            function(item);
+    DefenceScopeWriteLock(
+        [&]() {
+          for (auto &item : _map) {
+            if (predicate(item)) {
+              function(item);
+            }
           }
         },
-        writeLock);
+        _rwLock,
+        mode == OperationMode::WITH_LOCK);
   }
   template <typename Function>
   void doForKey(Function function, const Key &key, OperationMode mode = OperationMode::WITH_LOCK) const {
-    DEFENCE_SOCPE(auto item = _map.find(key); if (item != _map.end()) { function(*item); }, readLock);
+    DefenceScopeReadLock(
+        [&]() {
+          auto item = _map.find(key);
+          if (item != _map.end()) {
+            function(*item);
+          }
+        },
+        _rwLock,
+        mode == OperationMode::WITH_LOCK);
   }
   template <typename Function>
   void applyForKey(Function function, const Key &key, OperationMode mode = OperationMode::WITH_LOCK) {
-    DEFENCE_SOCPE(auto item = _map.find(key); if (item != _map.end()) { function(*item); }, writeLock);
+    DefenceScopeWriteLock(
+        [&]() {
+          auto item = _map.find(key);
+          if (item != _map.end()) {
+            function(*item);
+          }
+        },
+        _rwLock,
+        mode == OperationMode::WITH_LOCK);
   }
   template <typename Function>
   void insertOrDoIfExists(const Key &key, const Value &value, Function f) {
     if (contains(key)) {
       doForKey(f, key);
     } else {
-      Poco::ScopedWriteRWLock writeRWLock(_rwLock);
+      std::unique_lock<std::shared_timed_mutex> writeRWLock(_rwLock);
       if (contains(key, OperationMode::FORCE_NO_LOCK)) {
         doForKey(f, key, OperationMode::FORCE_NO_LOCK);
       } else {
@@ -154,7 +229,7 @@ class ConcurrentHashMap {
     if (contains(key)) {
       doForKey(f, key);
     } else {
-      Poco::ScopedWriteRWLock writeRWLock(_rwLock);
+      std::unique_lock<std::shared_timed_mutex> writeRWLock(_rwLock);
       if (contains(key, OperationMode::FORCE_NO_LOCK)) {
         doForKey(f, key, OperationMode::FORCE_NO_LOCK);
       } else {
--- main.cpp
+++ main.cpp
@@ -1,57 +1,67 @@
 #include <iostream>
-
+#include <thread>
 #include "ConcurrentHashMap.h"
 
 int main() {
-
   using TestMapType = ConcurrentHashMap<int, std::string>;
 
   TestMapType concurrentHashMap;
 
-  for (int i = 0; i < 10; ++i) {
+  for (int i = 0; i < 10000; ++i) {
     concurrentHashMap.insert(i, std::to_string(i));
   }
 
-  concurrentHashMap.doForEeach(
-      [](const TestMapType::ValueType &pair) {
-        std::cout << pair.first << " [" << pair.second << "]" << std::endl;
-      });
+  //  concurrentHashMap.doForEeach(
+  //      [](const TestMapType::ValueType &pair) {
+  //        std::cout << pair.first << " [" << pair.second << "]" << std::endl;
+  //      });
   std::cout << " --------------- " << std::endl;
-  concurrentHashMap.applyForEeach(
-      [](TestMapType::ValueType &pair) {
-        pair.second.append("0");
-        std::cout << pair.first << " [" << pair.second << "]" << std::endl;
-      });
-  std::cout << " --------------- " << std::endl;
-  concurrentHashMap.doForEeachIf(
-      [](const TestMapType::ValueType &pair) {
-        std::cout << pair.first << " [" << pair.second << "]" << std::endl;
-      },
-      [](const TestMapType::ValueType &pair) {
-        return pair.first % 2 == 0;
-      });
+
+  std::thread thr1([&concurrentHashMap]() { concurrentHashMap.applyForEeach([](TestMapType::ValueType &pair) { pair.second.append("+1"); }); });
+
+  std::thread thr2([&concurrentHashMap]() { concurrentHashMap.applyForEeach([](TestMapType::ValueType &pair) { pair.second.append("+2"); }); });
+
+  thr1.join();
+  thr2.join();
+
+  concurrentHashMap.doForEeach([](const TestMapType::ValueType &pair) { std::cout << pair.first << " [" << pair.second << "]" << std::endl; });
 
   std::cout << " --------------- " << std::endl;
-  concurrentHashMap.applyForEeachIf(
-      [](TestMapType::ValueType &pair) {
-        pair.second.append("-");
-        std::cout << pair.first << " [" << pair.second << "]" << std::endl;
-      },
-      [](TestMapType::ValueType &pair) {
-        return pair.first % 3 == 0;
-      });
 
-  std::cout << "size         : " << concurrentHashMap.size() << std::endl;
-  std::cout << "empty      ? : " << (concurrentHashMap.empty() ? "true" : "false") << std::endl;
-  std::cout << "contains 1 ? : " << (concurrentHashMap.contains(1) ? "true" : "false") << std::endl;
+  std::thread thr3([&concurrentHashMap]() {
+    concurrentHashMap.applyForEeachIf([](TestMapType::ValueType &pair) { pair.second.append("+even"); }, [](const TestMapType::ValueType &pair) { return pair.first % 2 == 0; });
+  });
 
-  concurrentHashMap.rwLock().writeLock();
-  concurrentHashMap.insert(11, std::to_string(11), TestMapType::OperationMode::FORCE_NO_LOCK);
-  concurrentHashMap.applyForEeach(
-      [](TestMapType::ValueType &pair) {
-        pair.second.append("0");
-        std::cout << pair.first << " [" << pair.second << "]" << std::endl;
-      }, TestMapType::OperationMode::FORCE_NO_LOCK);
-  concurrentHashMap.rwLock().unlock();
+  std::thread thr4([&concurrentHashMap]() {
+    concurrentHashMap.applyForEeachIf([](TestMapType::ValueType &pair) { pair.second.append("+not_even"); }, [](const TestMapType::ValueType &pair) { return pair.first % 2 != 0; });
+  });
+
+  thr3.join();
+  thr4.join();
+
+  concurrentHashMap.doForEeach([](const TestMapType::ValueType &pair) { std::cout << pair.first << " [" << pair.second << "]" << std::endl; });
+
+  //  std::cout << " --------------- " << std::endl;
+  //  concurrentHashMap.applyForEeachIf(
+  //      [](TestMapType::ValueType &pair) {
+  //        pair.second.append("-");
+  //        std::cout << pair.first << " [" << pair.second << "]" << std::endl;
+  //      },
+  //      [](TestMapType::ValueType &pair) {
+  //        return pair.first % 3 == 0;
+  //      });
+  //
+  //  std::cout << "size         : " << concurrentHashMap.size() << std::endl;
+  //  std::cout << "empty      ? : " << (concurrentHashMap.empty() ? "true" : "false") << std::endl;
+  //  std::cout << "contains 1 ? : " << (concurrentHashMap.contains(1) ? "true" : "false") << std::endl;
+  //
+  //  concurrentHashMap.rwLock().lock();
+  //  concurrentHashMap.insert(11, std::to_string(11), TestMapType::OperationMode::FORCE_NO_LOCK);
+  //  concurrentHashMap.applyForEeach(
+  //      [](TestMapType::ValueType &pair) {
+  //        pair.second.append("0");
+  //        std::cout << pair.first << " [" << pair.second << "]" << std::endl;
+  //      }, TestMapType::OperationMode::FORCE_NO_LOCK);
+  //  concurrentHashMap.rwLock().unlock();
   return 0;
 }
